b0dbf0b61b2bd4f9e7b2a7449d48f02ab7fdc5cd
cutthenblood <cutthenblood@gmail.com>
2014-07-09 12:16:46 +0400

    вернул аутентификацию

--- routes/deathmonitor/getReport.js
+++ routes/deathmonitor/getReport.js
@@ -10,7 +10,7 @@ var log = require('./../../log.js')(module);
 function isLoggedIn(req, res, next) {
 
     // if user is authenticated in the session, carry on
-    //if (req.isAuthenticated())
+    if (req.isAuthenticated())
         return next();
 
     // if they aren't redirect them to the home page
